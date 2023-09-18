abe14ad2d93870e23e003f74dd86856d86f688aa
daniel-lujan <daniel.lujan@udea.edu.co>
2023-09-18 13:27:45 -0500

    feat: :sparkles: Added schema validation
    Added Form schema and json validator decorator

---
+++ schemas/__init__.py
@@ -0,0 +1,19 @@
+from marshmallow import Schema, fields, validate
+
+
+class Form(Schema):
+    email = fields.Email(required = True)
+    name = fields.String(required = True, validate = validate.Length(min = 1, max = 30))
+    age = fields.Integer(required = True, validate = validate.Range(min = 1))
+    gender = fields.String(validate = validate.OneOf(("f", "m")))
+    favMusicGenres = fields.List(
+        fields.String(validate = validate.OneOf(("Reggaeton", "Pop", "Rap", "Rock"))),
+        required = True
+    )
+    moviesTaste = fields.Integer(required = True, validate = validate.Range(min = 0, max = 10))
+    # ...
+    mapPoint = fields.Dict(
+        fields.String(validate = validate.OneOf(("latitude", "altitude"))),
+        fields.Float(),
+        required = True
+    )
---
+++ schemas/validator.py
@@ -0,0 +1,21 @@
+from functools import wraps
+
+from flask import abort, request
+from marshmallow import Schema, ValidationError
+
+
+def validate_json(schema: type[Schema]):
+    def decorator(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                schema().load(data=request.json)
+            except ValidationError:
+                abort(400)
+            return func(*args, **kwargs)
+        
+        return wrapper
+    
+    return decorator    
+    return decorator
