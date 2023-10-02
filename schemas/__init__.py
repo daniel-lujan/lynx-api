29c68582aa35eabded8f7028da6845c9c376f6f3
daniel-lujan <daniel.lujan@udea.edu.co>
2023-10-01 20:17:24 -0500

    refactor: :safety_vest: Update form schema

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,18 +1,26 @@
 from marshmallow import Schema, fields, validate
 
-from schemas.constants import ALLOWED_MUSIC_GENRES
+from schemas.constants import ALLOWED_MUSIC_GENRES, PERSON_CHARACTERISTICS
 
 
 class Form(Schema):
-    email = fields.Email(required=True)
+    email = fields.Email(required=True, validate=validate.Length(min=1, max=50))
     name = fields.String(required=True, validate=validate.Length(min=1, max=30))
     age = fields.Integer(required=True, validate=validate.Range(min=1))
     gender = fields.String(validate=validate.OneOf(("f", "m")))
+    moviesTaste = fields.Integer(required=True, validate=validate.Range(min=0, max=10))
+    concertsTaste = fields.Integer(
+        required=True, validate=validate.Range(min=0, max=10)
+    )
+    partiesTaste = fields.Integer(required=True, validate=validate.Range(min=0, max=10))
     favMusicGenres = fields.List(
         fields.String(validate=validate.OneOf(ALLOWED_MUSIC_GENRES)), required=True
     )
-    moviesTaste = fields.Integer(required=True, validate=validate.Range(min=0, max=10))
-    # ...
+    mostImportantAttr = fields.String(validate=validate.OneOf(PERSON_CHARACTERISTICS))
+    catsOrDogs = fields.Integer(required=True, validate=validate.OneOf((0, 1)))
+    messiOrCristiano = fields.Integer(required=True, validate=validate.OneOf((0, 1)))
+    backOrFront = fields.Integer(required=True, validate=validate.OneOf((0, 1)))
+    marvelOrDC = fields.Integer(required=True, validate=validate.OneOf((0, 1)))
     mapPoint = fields.Dict(
         fields.String(validate=validate.OneOf(("lat", "lng"))),
         fields.Float(),
--- schemas/constants.py
+++ schemas/constants.py
@@ -1,1 +1,7 @@
 ALLOWED_MUSIC_GENRES = ("Reggaeton", "Pop", "Rap", "Rock")
+PERSON_CHARACTERISTICS = (
+    "Personalidad",
+    "Físico",
+    "Inteligencia",
+    "Dinero",
+)
